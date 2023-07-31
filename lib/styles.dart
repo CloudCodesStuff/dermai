60221fe2091f27bfe6e9d2f7c030b18e512c6bc6
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2023-07-31 13:17:25 -0400

    first commit

---
+++ lib/styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+const kMainFont = 'Outfit';
+const kButtonFont = 'Outfit';
+const kDisplayFont = 'Outfit';
+
+const kColorGreen = Color.fromARGB(255, 255, 255, 255);
+const kColorLightGreen = Color(0xFF0f0f0f);
+const kColorBrown = Color.fromARGB(255, 31, 33, 34);
+const kColorLightYellow = Color.fromARGB(255, 255, 255, 255);
+
+const kColorRed = Color(0xFFD96666);
+const kColorLightRed = Color(0xFFF2CECE);
+const kColorLightGray = Color.fromARGB(255, 0, 0, 0);
+
+const kColorHunterGreen = Color(0xFF386641);
+const kColorMayGreen = Color(0xFF6a994e);
+const kColorAndroidGreen = Color.fromARGB(255, 0, 0, 0);
+const kColorEggshell = Color.fromARGB(255, 0, 0, 0);
+const kColorBitterSweetShimmer = Color(0xFFbc4749);
+
+const kBgColor = kColorGreen;
+
+const kTitleTextStyle = TextStyle(
+  fontFamily: kDisplayFont,
+  fontSize: 45.0,
+  color: kColorAndroidGreen,
+  decoration: TextDecoration.none,
+);
+
+const kAnalyzingTextStyle = TextStyle(
+    fontFamily: kMainFont,
+    fontSize: 25.0,
+    color: kColorAndroidGreen,
+    decoration: TextDecoration.none);
+
+const kResultTextStyle = TextStyle(
+    fontFamily: kDisplayFont,
+    fontSize: 35.0,
+    color: kColorAndroidGreen,
+    decoration: TextDecoration.none);
+
+const kResultRatingTextStyle = TextStyle(
+    fontFamily: kMainFont,
+    fontSize: 18.0,
+    color: Colors.black,
+    decoration: TextDecoration.none);
---
+++ lib/widget/waste_photo_view.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+import '../styles.dart';
+
+class WastePhotoView extends StatelessWidget {
+  final File? file;
+  const WastePhotoView({super.key, this.file});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Container(
+          //width: 300,
+          decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.all(Radius.circular(5))),
+          height: 300,
+          //color: Colors.blueGrey,
+          child: (file == null)
+              ? _buildEmptyView()
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(5), // Image border
+                  child: Image.file(file!, fit: BoxFit.cover),
+                )),
+    );
+  }
+
+  Widget _buildEmptyView() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.photo,
+            size: 40,
+            color: kColorLightGray,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Please select a photo',
+            style: kAnalyzingTextStyle,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      )),
+    );
+  }
+}
